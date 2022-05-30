1cc81702366f4cd755bae0c761fc722eecb7e3c8
FabinhuSilva <fabiano.fesilva@gmail.com>
2022-05-29 23:38:36 -0300

    Concluido vetor multiplicado pelo maior

--- src/vetorcom20divisao/VetorCom20Divisao.java
+++ src/vetorcom20divisao/VetorCom20Divisao.java
@@ -13,14 +13,17 @@ public class VetorCom20Divisao {
     public static void main(String[] args) {
         
         
-        int vetorCom20[]={2,67,5,15,7,3,45}; //possui 6 elementos atualmente
+        int vetorCom20[]={2,67,4,13,5,15,7,3,45,99}; //possui 6 elementos atualmente
         int maiorValor=0;
         
         for(int contador=0;contador < vetorCom20.length;contador++){
             if(vetorCom20[contador] > maiorValor)
                 maiorValor = vetorCom20[contador];
           }
-        System.out.println(maiorValor);
+        System.out.println("O Maior Valor do Vetor é "+maiorValor);
+        for(int contFor=0; contFor < vetorCom20.length;contFor++){
+            System.out.println("Posicao "+contFor+" Vetor o Valor é "+vetorCom20[contFor]+" X "+maiorValor+" = "+vetorCom20[contFor]*maiorValor);
+        }
     }
     
 }
